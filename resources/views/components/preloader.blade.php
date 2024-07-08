a7a4b86a38e60e72e648a61500bc1b8424d75319
Vladislav Ometov <vladislavometov@MacBook-Air-Vladislav.local>
2024-07-08 14:39:15 +0700

    Add logo

--- resources/views/components/preloader.blade.php
+++ resources/views/components/preloader.blade.php
@@ -1,8 +1,9 @@
-<script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+{{--<script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>--}}
 
 <div class="preloader">
-  <div class="logo">
-    <dotlottie-player src="json/AnimatedLogoHorizontal.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>
-  </div>
+{{--  <div class="logo">--}}
+{{--    <dotlottie-player src="json/AnimatedLogoHorizontal.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>--}}
+{{--  </div>--}}
+  @include('components.logo')
   <div class="progressBar"></div>
 </div>
